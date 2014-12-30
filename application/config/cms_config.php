dab47abd7c7698701f04b86e0151c1caab219941
Abdulrezak <abdulrazaq_18@hotmail.com>
2014-12-30 16:14:37 +0000

    removed log files and updated cms_config file

--- application/config/cms_config.php
+++ application/config/cms_config.php
@@ -21,25 +21,15 @@ $config['hash_function'] = 'hash';
 
 $config['installed'] = true;
 
+$config['color_picker'] = false;
+
+$config['color'] = 'orange';
+
+//$config['def_package'] = 1;
+
 $config['captcha_disabled'] = false;
 
 $config['admin_beginner_enabled'] = true;
 
 $config['all_results_default'] = true;
-$config['version'] = '';
-
-$config['default_template_css'] = 'assets/css/bootstrap.min.css';
-
-$config['installed'] = false;
-
-$config['color_picker'] = true;
-
-$config['color'] = 'purple';
-
-$config['def_package'] = 1;
-
-$config['captcha_disabled'] = FALSE;
-
-$config['all_results_default'] = TRUE;
-
 
